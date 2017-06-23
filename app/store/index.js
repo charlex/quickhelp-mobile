732af90f2889181e83f4194640013bea2cc9bfd6
charlex <charley@bodkin.me>
2017-06-23 08:56:03 -0700

    fixing tests

--- app/store/index.js
+++ app/store/index.js
@@ -2,9 +2,10 @@
  * Store index
  */
 
-import reducers from '../reducers';
 import { createLogger } from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
+
+import reducers from '../reducers';
 
 const loggerMiddleware = createLogger();
 
